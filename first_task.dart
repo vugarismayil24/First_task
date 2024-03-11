6fd2c54f2cc568a680f1b6e796604a6f48b76fba
vugarismayil24 <163012262+vugarismayil24@users.noreply.github.com>
2024-03-11 18:38:33 +0400

    Add files via upload

---
+++ first_task.dart
@@ -0,0 +1,26 @@
+void main() {
+    //DateTime datenow = DateTime(2024);
+    //DateTime birthdayYear = DateTime(2001);
+   
+    //print(datenow.year - birthdayYear.year);
+    
+
+    //double celci = 20;
+    
+
+    //double tempratur = celci * 1.8 + 32;
+
+    //print(tempratur);
+
+    int eni = 20;
+    int uzunlugu = 30;
+    
+    int perimetr = 2 * (eni + uzunlugu);
+
+    print(perimetr);
+
+    int sahe = eni * uzunlugu;
+
+    print(sahe);
+    
+}
